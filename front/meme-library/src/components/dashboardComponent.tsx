2ab6f3cff7a60388cda97ce1bacf34f03ac98f6b
Marwa <majbrim@gmail.com>
2021-05-10 17:01:46 +0200

    added config node

--- front/meme-library/src/components/dashboardComponent.tsx
+++ front/meme-library/src/components/dashboardComponent.tsx
@@ -1,82 +1,18 @@
-import { useState } from 'react';
-import uploadFileToBlob, { isStorageConfigured } from '../azure-storage-blob';
-import Path from 'path';
-
-const storageConfigured = isStorageConfigured();
-
 const DashboardPage = (): JSX.Element => {
-  // all blobs in container
-  const [blobList, setBlobList] = useState<string[]>([]);
 
-  // current file to upload into container
-  const [fileSelected, setFileSelected] = useState(null);
+  return(
 
-  // UI/form management
-  const [uploading, setUploading] = useState(false);
-  const [inputKey, setInputKey] = useState(Math.random().toString(36));
-
-  const onFileChange = (event: any) => {
-    // capture file into state
-    setFileSelected(event.target.files[0]);
-  };
-
-  const onFileUpload = async () => {
-    // prepare UI
-    setUploading(true);
-
-    // *** UPLOAD TO AZURE STORAGE ***
-    const blobsInContainer: string[] = await uploadFileToBlob(fileSelected);
-
-    // prepare UI for results
-    setBlobList(blobsInContainer);
-
-    // reset state/form
-    setFileSelected(null);
-    setUploading(false);
-    setInputKey(Math.random().toString(36));
-  };
-
-  // display form
-  const DisplayForm = () => (
-    <form action="" className='formUser'> 
-       <h2 className='title-meme-dashbord'>Choose a Meme</h2>
-       <div>
-       <input className='input-picture' type="file" name='picture' onChange={onFileChange} key={inputKey || ''}/>
-       </div>
-       <div>
-       <button className="user-btn" type="submit" onClick={onFileUpload} >Submit</button>
-       </div>
-   </form>
-  )
-
-  // display file name and image
-  const DisplayImagesFromContainer = () => (
-    <div className="App">
-      <section className="Memes-container">
+    <form action="http://localhost:3500/upload" className='formUser' method="post" encType="multipart/form-data"> 
+      <h2 className='title-meme-dashbord'>Choose a Meme</h2>
       <div className="Memes-img">
-        {blobList.map(item => {
-          return ( 
-                
-            <img key={item} src={item} alt={item}/>
-              
-            
-          );
-        })}
-        </div>
-      </section>
-    </div>
-  );
- 
-      
-  return (
-    <div>
-      
-      {storageConfigured && !uploading && DisplayForm()}
-      {storageConfigured && uploading && <div id="uploading">Uploading</div>}
-      {storageConfigured && blobList.length > 0 && DisplayImagesFromContainer()}
-      {!storageConfigured && <div>Storage is not configured.</div>}
-    </div>
-  );
-};
+      <input className='input-picture' type="file" name='keyform'/>
+      <input type="hidden" name="user_id" value="1" />
+      </div>
+      <div>
+      <button className="user-btn" type="submit">Submit</button>
+      </div>
+    </form>
+  )
+}
 
 export default DashboardPage;
